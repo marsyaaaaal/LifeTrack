2f5af87b74a64fa2a23202f0f5e1b74c87759a4b
Marcial Abasola <marcial.abasolajr18@gmail.com>
2024-08-11 00:15:12 +0800

    added default(current date) date value for calendar

--- src/components/calendar.tsx
+++ src/components/calendar.tsx
@@ -1,30 +1,9 @@
-import dayjs from "dayjs";
 import { useState } from "react";
-import { Calendar } from "@mantine/dates";
+import { DatePicker } from "@mantine/dates";
 
 function Calenda() {
-  const [selected, setSelected] = useState<Date[]>([]);
-  const handleSelect = (date: Date) => {
-    const isSelected = selected.some((s) => dayjs(date).isSame(s, "date"));
-    console.log(isSelected);
-    if (isSelected) {
-      setSelected((current) =>
-        current.filter((d) => !dayjs(d).isSame(date, "date"))
-      );
-    } else if (selected.length < 1) {
-      setSelected((current) => [...current, date]);
-    }
-    console.log(date);
-  };
-
-  return (
-    <Calendar
-      getDayProps={(date) => ({
-        selected: selected.some((s) => dayjs(date).isSame(s, "date")),
-        onClick: () => handleSelect(date),
-      })}
-    />
-  );
+  const [value, setValue] = useState<Date | null>(new Date());
+  return <DatePicker allowDeselect value={value} onChange={setValue} />;
 }
 
 export default Calenda;
